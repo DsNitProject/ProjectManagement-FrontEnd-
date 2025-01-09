deb7de221de630900eb2204ecaf80a275d6bcd77
Amir-m-Arabi <arabiamir2005@gmail.com>
2025-01-09 13:13:02 +0330

    Menu Load Corresponding to the Registration Section.

---
+++ binnocup/binnocup.js
@@ -0,0 +1,7 @@
+const $ = document
+let registerOrLogin = $.getElementById("reglog")
+let registerOrLoginBtn = $.getElementById("RL-btn")
+
+registerOrLogin.addEventListener("click" , ()=>{
+    registerOrLoginBtn.style.display = ""
+})
