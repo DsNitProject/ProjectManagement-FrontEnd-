17470d686e479db2f1f14b442296b076816b7802
Amir-m-Arabi <arabiamir2005@gmail.com>
2025-01-19 01:19:59 +0330

    The footer element was added to it

--- my-app/src/task.js
+++ my-app/src/task.js
@@ -12,6 +12,27 @@ const TaskDetailsPage = () => {
           </span>
         </div>
       </header>
+      <footer className="w-full bg-slate-800 text-white p-6 mt-[9.95rem]">
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
+          <ul className="space-y-2">
+            <li className="-ml-3">Contact Us</li>
+            <li style={{ listStyle: 'disc' }}>About Us</li>
+            <li style={{ listStyle: 'disc' }}>Site Management</li>
+          </ul>
+          <ul className="space-y-2">
+            <li className="-ml-3">Information Base</li>
+            <li style={{ listStyle: 'disc' }}>Ministry of Science, Research and Technology</li>
+            <li style={{ listStyle: 'disc' }}>Vice President for Science and Technology</li>
+          </ul>
+          <ul className="space-y-2">
+            <li className="-ml-3">Secretariat Address</li>
+            <li style={{ listStyle: 'disc' }}>
+              Address: Babol Noshirvani University of Technology
+            </li>
+            <li style={{ listStyle: 'disc' }}>Phone: 09370117891 / 09035912910</li>
+          </ul>
+        </div>
+      </footer>
     </div>
   );
 };
